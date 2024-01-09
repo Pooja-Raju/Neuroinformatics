f1baa5dd88dbba7312e4ab37c63e08fea896204f
Pooja-Raju <r.pooja@research.iiit.ac.in>
2024-01-09 20:27:39 +0530

    assignment0.m

--- Neuroinformatics/InClass_assignment/assignment0.m
+++ Neuroinformatics/InClass_assignment/assignment0.m
@@ -13,11 +13,21 @@ y = Power_data_transpose(:,2);
 [h,p,ci,stats] = ttest2(x,y)
 
 %Question2:Creating 4 individuals sine waves and a combined wave 
-%sampling frequency
-Fs = 1000;
-t = [0:0.1:2*pi]
-a = sin(t);
-plot(t,a)
 
-A = sin(2*pi*2*t)
-plot(A)
+t = [0:0.001:4];
+A = [7 2 5 4];
+Phase = [pi/3 pi/2 -pi/3 pi/4];
+F = [2 8 12 25];
+figure;
+for i = 1:4
+    x(i,:) = A(i)*sin(2*pi*F(i)*t+Phase(i));
+    subplot(5,1,i)
+    plot(x(i,:))
+    xlim([0 4000])
+end
+
+y = mean(x,1);
+subplot(5,1,5)
+plot(y)
+xlim([0 4000])
+%
